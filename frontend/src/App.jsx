56e80a9db6771a15bf1cab53acb507d87d1d8c7d
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-11 14:44:16 +0100

    adding required pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-green-500">
-      Testing Tailwinf
+    <div >
     </div>
   )
 }
---
+++ frontend/src/pages/MyAppointement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyAppointement = () => {
+  return (
+    <div>MyAppointement</div>
+  )
+}
+
+export default MyAppointement
