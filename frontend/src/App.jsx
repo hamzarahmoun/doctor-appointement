38a2b9f828f14a80ffb31b7e8c771f721966b965
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-11 14:55:33 +0100

    adding all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+import MyAppointement from './pages/MyAppointement'
+import Appointement from './pages/Appointement'
 
 const App = () => {
   return (
-    <div >
+    <div className='mx-4 sm:mx[10%] '>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointement' element={<MyAppointement />} />
+        <Route path='/my-appointement/:docId' element={<Appointement />} />
+
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Appointement.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Appointement = () => {
+  return (
+    <div>Appointement</div>
+  )
+}
+
+export default Appointement
