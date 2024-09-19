2cd54962fd7b5d642f990072bdf3dae5aee4ec24
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-19 22:43:58 +0100

    top doctors

--- frontend/src/components/TopDoctor.jsx
+++ frontend/src/components/TopDoctor.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { doctors } from '../assets/assets_frontend/assets'
 
 const TopDoctor = () => {
   return (
     <div>
-        
+        <h1>Top Doctors to Book</h1>
+        <p>Simply browse through our extensive list of trusted doctors</p>
+        <div>
+            {doctors.slice(0,10).map((items,index)=>(
+                <div>
+                    <img src={items.image} alt="" />
+                    <div>
+                        <div>
+                            <p></p><p>Available</p>
+                        </div>
+                        <p>{items.name}</p>
+                        <p>{items.speciality}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
