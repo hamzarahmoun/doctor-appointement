0eded5153cf8d3d8fc3b7748b358c93e10330d96
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-20 18:54:11 +0100

    banner function

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+    const navigate = useNavigate()
   return (
     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
         {/*.......left side ......*/}
@@ -10,7 +12,7 @@ const Banner = () => {
                     <p>Book Appointment</p>
                     <p className='mt-4'>With 100 Trusted Doctors</p>
                 </div>
-                <button className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all '>Create Account </button>
+                <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all '>Create Account </button>
             </div>
         {/*........right side ........*/}
         <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
