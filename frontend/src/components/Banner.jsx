fd87105d4f5e291727fd7ba5056fadcf5355b334
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-20 18:51:22 +0100

    banner css

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Banner = () => {
   return (
-    <div>
+    <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
         {/*.......left side ......*/}
-            <div>
-                <div>
+            <div className='flex-1 py-8 sm:py-16 lg:py-24 lg:pl-5'>
+                <div className='text-xl sm:text-2xl ms:text-3xl lg:text-5xl font-semibold text-white'>
                     <p>Book Appointment</p>
-                    <p>With 100 Trusted Doctors</p>
+                    <p className='mt-4'>With 100 Trusted Doctors</p>
                 </div>
-                <button>Create Account </button>
+                <button className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all '>Create Account </button>
             </div>
         {/*........right side ........*/}
-        <div>
-            <img src={assets.appointment_img} alt="" />
+        <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
+            <img className='w-full absolute bottom-0 right-0 max-w-md ' src={assets.appointment_img} alt="" />
         </div>
          
     </div>
