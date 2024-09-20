a6725a6d743e4259e9fe6a2d9f6096ab32f10cf3
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-20 18:30:29 +0100

    topdoctor function

--- frontend/src/components/TopDoctor.jsx
+++ frontend/src/components/TopDoctor.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { doctors } from '../assets/assets_frontend/assets'
-
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {AppContext} from '../context/AppContext'
 const TopDoctor = () => {
+    const navigate = useNavigate();
+    const {doctors} = useContext(AppContext);
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-6'>
             {doctors.slice(0,10).map((items,index)=>(
-                <div key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                <div onClick={()=>navigate(`/appontement/${items._id}`)} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
                     <img className='bg-blue-50' src={items.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -20,6 +22,7 @@ const TopDoctor = () => {
                 </div>
             ))}
         </div>
+        <button onClick={()=>{navigate('/doctors');scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
