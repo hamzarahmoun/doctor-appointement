ff54454907ec96084d5b3ba293353b1793df0546
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-20 19:17:43 +0100

    footer css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,22 +3,24 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/*........ left side .......*/}
 
             <div>
-                <img src={assets.logo} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors,
-                shedulde your appoitement hassle-free</p>
+                <img className='mb-5 w-40' src={assets.logo} alt="" />
+                <p className='w-full md:2/3 text-gray-600 leading-6'>Simply browse through our extensive list of trusted doctors,
+                shedulde your appoitement hassle-free Je tiens à exprimer toute ma gratitude pour les opportunités et les expériences enrichissantes
+                que j'ai eues au sein de l'entreprise. Ces mois passés dans votre équipe ont été 
+                une période précieuse d'apprentissage et de développement professionnel</p>
 
             </div>
 
              {/*........ center side .......*/}
 
             <div>
-                <p>Company</p>
-                <ul>
+                <p className='text-xl font-medium mb-5 '>Company</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Contact Us</li>
@@ -28,8 +30,8 @@ const Footer = () => {
              {/*........ right side .......*/}
 
             <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5 ' >GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+212 645878596</li>
                     <li>hamza@gmail.com</li>
                 </ul>
@@ -40,7 +42,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p>Copyright 2024@ Bugfa - All Right Reserved </p>
+            <p className='py-5 text-sm text-center' >Copyright 2024@ Bugfa - All Right Reserved </p>
 
         </div>
 
