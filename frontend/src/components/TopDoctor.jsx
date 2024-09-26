d434ddf5c0579d994e1d1e8aefe8c5d5a2fb2eed
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-26 13:05:13 +0100

    top doctor scroll

--- frontend/src/components/TopDoctor.jsx
+++ frontend/src/components/TopDoctor.jsx
@@ -10,7 +10,7 @@ const TopDoctor = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-6'>
             {doctors.slice(0,10).map((items,index)=>(
-                <div onClick={()=>navigate(`/appontement/${items._id}`)} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+                <div onClick={()=>{navigate(`/appointement/${items._id}`); scroll(0,0)}} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
                     <img className='bg-blue-50' src={items.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
