2717d0180ae9001a41fc304605838a629bcb2c24
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-26 13:13:18 +0100

    about us structure

--- frontend/src/components/RelatedDoctor.jsx
+++ frontend/src/components/RelatedDoctor.jsx
@@ -14,7 +14,7 @@ const RelatedDoctor = ({speciality,docId}) => {
     },[doctors,speciality,docId])
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-        <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+        <h1 className='text-3xl font-medium'>Related Doctors</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-6'>
             {relDoc.slice(0,5).map((items,index)=>(
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <div>
+        <p>About <span>Us</span></p>
+      </div>
+      <div>
+        <img src={assets.about_image} alt="" />
+        <div>
+          <p>
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque efficitur, nisl et cursus blandit, nunc metus scelerisque purus, eu tincidunt nisl nulla a arcu. Morbi ut consequat nunc. Nullam tincidunt elit nec est pellentesque, at tempus ligula bibendum. Sed et orci nec augue luctus consequat. Suspendisse potenti. Vivamus bibendum nunc id turpis fringilla, at dignissim justo elementum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque efficitur, nisl et cursus blandit, nunc metus scelerisque purus, eu tincidunt nisl nulla a arcu. Morbi ut consequat nunc. Nullam tincidunt elit nec est pellentesque, at tempus ligula bibendum. Sed et orci nec augue luctus consequat. Suspendisse potenti. Vivamus bibendum nunc id turpis fringilla, at dignissim justo elementum.</p>
+          
+          <b>Our Vision</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque efficitur, nisl et cursus blandit, nunc metus scelerisque purus, eu tincidunt nisl nulla a arcu. Morbi ut consequat nunc. Nullam tincidunt elit nec est pellentesque, at tempus ligula bibendum. Sed et orci nec augue luctus consequat. Suspendisse potenti. Vivamus bibendum nunc id turpis fringilla, at dignissim justo elementum.</p>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
