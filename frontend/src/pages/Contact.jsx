3c38c06f7e8956325c885366a7addf6e61fc4a0a
hamzarahmoun <hamza.rahmoun456@gmail.com>
2024-09-27 12:47:36 +0100

    contact us page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,25 @@
-    import React from 'react'
-    
-    const Contact = () => {
-      return (
-        <div></div>
-      )
-    }
-    
-    export default Contact
+import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
+
+const Contact = () => {
+  return (
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>Contact <span className='text-gray-700 font-medium'>Us</span></p>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit</p>
+          <p className='text-gray-600'>Tel: (212) 666-0987 <br /> Email: hamza@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>Careers at Prescript</p>
+          <p className='text-gray-600'>Learn about more about our projects</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
